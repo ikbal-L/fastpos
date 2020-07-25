69eeaeb096dd1e1c827775400b1bf575def1d227
Othmani Samir <othmani.samir@gmail.com>
2020-07-25 13:29:25 +0100

    Upadte UI

---
+++ PosTest/PosTest/ViewModels/OpenTableViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+using ServiceInterface.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace PosTest.ViewModels
+{
+    public class OpenTableViewModel: Screen
+    {
+        private Order _selectedOrder;
+
+        public ICollection<Order> TableOrders { get;set;}
+
+        public Order SelectedOrder
+        {
+            get { return _selectedOrder; }
+            set
+            {
+                _selectedOrder = value;
+                NotifyOfPropertyChange(() => SelectedOrder);
+            }
+        }
+
+        public OpenTableViewModel(IList<Order> orders, ServiceInterface.Model.Table table)
+        {
+            TableOrders = orders.Where<Order>(o => o.Table.Id == table.Id).ToList();
+        }
+
+
+        public void OpenSelectedCommand()
+        {
+            //Action To Return Chechout View
+            throw new NotImplementedException();
+        }
+
+    }
+}
