f6f1f3c83cc3aa1114de007701644d0b3f2d03c5
Othmani Samir <othmani.samir@gmail.com>
2020-08-10 18:54:06 +0100

    Update laste new viewcheck out

---
+++ PosTest/PosTest/Converters/ObjectToBoolConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using ServiceInterface.Model;
+
+namespace PosTest.Converters
+{
+    class ObjectToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is OrderItem)
+            {
+                if (value is OrderItem item && item.Product is Platter && (item.Product as Platter).Additives != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (value!=null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
