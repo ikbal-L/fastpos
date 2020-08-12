1c54e8ff4d31e53e9494d624e5685034dc9de0dd
Othmani Samir <othmani.samir@gmail.com>
2020-08-12 11:45:04 +0100

    new view

---
+++ PosTest/PosTest/Converters/BooleanToHiddenVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using ServiceInterface.Model;
+
+namespace PosTest.Converters
+{
+    class BooleanToHiddenVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((bool)value == false)
+                return Visibility.Hidden;
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
