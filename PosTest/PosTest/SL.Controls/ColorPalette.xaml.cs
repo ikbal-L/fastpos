d00e0468cde96f6fd06d56649f49b1446aa9996d
Othmani Samir <othmani.samir@gmail.com>
2020-09-22 11:39:11 +0100

    Add Color Palette User Controle & Update EditProduct UserControl

---
+++ PosTest/PosTest/SL.Controls/ColorPalette.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PosTest.SL.Controls
+{
+    /// <summary>
+    /// Interaction logic for ColorPalette.xaml
+    /// </summary>
+    public partial class ColorPalette : UserControl
+    {
+        public SolidColorBrush CPColorBrush
+        {
+            get { return (SolidColorBrush)GetValue(CPColorBrushProperty); }
+            set { SetValue(CPColorBrushProperty, value); }
+        }
+
+        public static readonly DependencyProperty CPColorBrushProperty =
+            DependencyProperty.Register("CPColorBrush", typeof(string),
+                typeof(ColorPalette), new PropertyMetadata(""));
+        public ColorPalette()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
