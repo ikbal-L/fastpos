728f99e6ae98cdf2cac90e4f387e69cdd72ef5fd
Othmani Samir <othmani.samir@gmail.com>
2020-09-23 12:45:01 +0100

    Show Button Is Mouse Over in Parent And null Additives

---
+++ PosTest/WpfApp1/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp1
+{
+    public class Person: INotifyPropertyChanged
+    {
+        private string name;
+
+        public Person()
+        {
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (value != this.name)
+                {
+                    this.name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
