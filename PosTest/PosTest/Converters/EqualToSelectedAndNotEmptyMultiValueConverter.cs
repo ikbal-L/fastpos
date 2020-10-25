1c8edc9c0895e76f79a0bd8a5de312cf087da0ea
Iqbal Lachraf <prog2.71828@gmail.com>
2020-10-25 09:04:37 +0100

    Check icon appears only when item equals selected item and Description is not null or empty in AdditivesSettingsViewModel

---
+++ PosTest/PosTest/Converters/EqualToSelectedAndNotEmptyMultiValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PosTest.Converters
+{
+    class EqualToSelectedAndNotEmptyMultiValueConverter: IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var selected = values[0];
+            var obj = values[1];
+            var str = values[2] as string;
+            return (selected.Equals(obj) && !string.IsNullOrEmpty(str));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
