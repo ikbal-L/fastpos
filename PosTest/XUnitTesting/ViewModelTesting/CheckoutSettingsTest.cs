6fa303914594e4c7b00294864aa294c7c7904b45
Iqbal Lachraf <prog2.71828@gmail.com>
2020-10-31 13:34:32 +0100

    Extract methods to a helper class

--- PosTest/XUnitTesting/ViewModelTesting/CheckoutSettingsTest.cs
+++ PosTest/XUnitTesting/ViewModelTesting/CheckoutSettingsTest.cs
@@ -38,7 +38,7 @@ namespace XUnitTesting.ViewModelTesting
                         categoryServiceStatusCode = 200;
                         productServiceStatusCode = 200;
                     }
-                )).Returns((GetAllCategories(), GetAllProducts()));
+                )).Returns((MockingHelpers.GetAllCategories(), MockingHelpers.GetAllProducts()));
 
 
             _productService.Setup((ps => ps.UpdateProduct(new Product()))).Returns(200);
@@ -50,25 +50,6 @@ namespace XUnitTesting.ViewModelTesting
 
         public delegate void GetGetAllProductsCallback(ref int categStatusCode,
             ref int prodStatusCode);
-
-        public static IEnumerable<Product> GetAllProducts()
-        {
-            yield return new Product() {Id = 5, Rank = 6};
-            yield return new Product() {Id = 2, Rank = 18};
-            yield return new Product() {Id = 17, Rank = 6};
-            yield return new Product() {Id = 23, Rank = 27};
-        }
-
-        public static IEnumerable<Category> GetAllCategories()
-        {
-            yield return new Category() {Id = 05, Rank = 08, Name = "Cat1"};
-            yield return new Category() {Id = 02, Rank = 18, Name = "Cat2"};
-            yield return new Category() {Id = 17, Rank = 06, Name = "Cat3"};
-            yield return new Category() {Id = 23, Rank = 27, Name = "Cat4"};
-            yield return new Category() {Id = 08, Rank = 24, Name = "Cat5"};
-            yield return new Category() {Id = 19, Rank = null, Name = "Cat6"};
-            yield return new Category() {Id = 14, Rank = null, Name = "Cat7"};
-        }
 
         [Fact]
         public void PutCategoryInCellOf_BothCategoriesFromCurrentCategoriesList_PermutateCategories()
