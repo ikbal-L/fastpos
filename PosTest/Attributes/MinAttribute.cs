1cf5f7715c21d1555d89e617ba5b142d4b131e8d
Lachraf Iqbal <prog2.71828@gmail.com>
2021-04-24 04:42:24 +0100

    Small Refactor

---
+++ PosTest/Attributes/MinAttribute.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Attributes
+{
+
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Parameter | System.AttributeTargets.Property)]
+    public class MinAttribute: ValidationAttribute
+    {
+        public double DoubleMinValue { get; }
+        public decimal DecimalMinValue { get; }
+
+        public MinAttribute(string decimalValue)
+        {
+            DecimalMinValue = decimal.Parse(decimalValue);
+        }
+
+        public MinAttribute(double doubleMinValue)
+        {
+            DoubleMinValue = doubleMinValue;
+        }
+
+        public string GetErrorMessage(object obj, string displayName = "") => $"Value of {displayName} must be Greater  than {obj}.";
+
+
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            
+            if(value is double doubleValue && doubleValue <= DoubleMinValue)
+            {
+                return  new ValidationResult(GetErrorMessage(DoubleMinValue,validationContext.DisplayName));
+            }
+
+            if (value is decimal decimalValue && decimalValue <= DecimalMinValue)
+            {
+                return new ValidationResult(GetErrorMessage(DecimalMinValue,validationContext.DisplayName));
+            }
+            
+            return ValidationResult.Success;
+        }
+    }
+
+    public class DecimalAttribute : ValidationAttribute
+    {
+
+        public string GetErrorMessage(object obj,string displayName ="") => $"Value of {displayName} must be of Type {nameof(Decimal)}.";
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            
+            if (value is string s && decimal.TryParse(s,out  var d))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(GetErrorMessage(value, validationContext.DisplayName));
+        }
+    }
+}
