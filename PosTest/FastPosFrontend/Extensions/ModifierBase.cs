a25957c0a9146e38e56d9e51eabbe3b7531b8981
Lachraf Iqbal <prog2.71828@gmail.com>
2021-04-26 13:25:24 +0100

    Add Utilities

---
+++ PosTest/FastPosFrontend/Extensions/ModifierBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace FastPosFrontend.Extensions
+{
+    public abstract class ModifierBase
+    {
+        public abstract void Apply(DependencyObject target);
+    }
+}
---
+++ PosTest/FastPosFrontend/Extensions/ModifierCollection.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FastPosFrontend.Extensions
+{
+    public class ModifierCollection : Collection<Modifier>
+    { }
+}
