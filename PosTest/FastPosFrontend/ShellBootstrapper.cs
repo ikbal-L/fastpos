51ad913ff177d2e6667d18ac476c87cea7e6a64e
user2-71828 <70612927+user2-71828@users.noreply.github.com>
2021-07-26 10:01:11 +0100

    Start the backend server from java process

--- PosTest/FastPosFrontend/ShellBootstrapper.cs
+++ PosTest/FastPosFrontend/ShellBootstrapper.cs
@@ -48,70 +48,50 @@ namespace FastPosFrontend
             //splashScreen.Close();
 
 
-            //var databaseProcess = Process.Start(@"C:\xampp\mysql_start.bat");
-            //databaseProcess.StartInfo.UseShellExecute = false;
-            //databaseProcess.StartInfo.CreateNoWindow = true;
-            //ReadProcessId();
-            //var databaseProcess = new Process()
-            //{
-            //    StartInfo = new ProcessStartInfo()
-            //    {
-            //        WorkingDirectory = @"C:\xampp",
-            //        FileName = @"C:\xampp\mysql_start.bat",
-            //        Arguments = "bootrun",
-            //        ErrorDialog = true,
-            //        CreateNoWindow = true,
-            //        UseShellExecute = false,
 
-            //    },
-            //};
 
-            //_backendServerProcess = new Process()
-            //{
-            //    StartInfo = new ProcessStartInfo()
-            //    {
-            //        WorkingDirectory = @"C:\Users\User\source\fastpos-backend",
-            //        FileName = @"C:\Users\User\source\fastpos-backend\gradlew.bat",
-            //        Arguments = "bootrun",
-            //        ErrorDialog = true,
-            //        CreateNoWindow = true,
-            //        UseShellExecute = false
+            _backendServerProcess = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "java",
+                    Arguments = "-jar model-0.0.1-SNAPSHOT.jar",
+                    ErrorDialog = true,
+                    CreateNoWindow = true,
+                    UseShellExecute = false
 
-            //    }
-            //};
-            //if (!ConnectionHelper.PingHost(portNumber: 3306))
-            //{
-            //    databaseProcess?.Start();
-            //}
+                }
+            };
 
-            //if (!ConnectionHelper.PingHost())
-            //{
-            //    _backendServerProcess.Start();
-            //    //WriteProcessId(_backendServerProcess.Id);
-            //}
+            if (!ConnectionHelper.PingHost())
+            {
+                _backendServerProcess.Start();
+                
+            }
 
-            //var stopWatch = Stopwatch.StartNew();
-            //stopWatch.Start();
-            //while (!ConnectionHelper.PingHost())
-            //{
-            //    if (stopWatch.ElapsedMilliseconds >= 30000)
-            //    {
-            //        stopWatch.Stop();
-            //        //backendServerProcess.Kill();
-            //        break;
-            //    }
-            //}
+            var stopWatch = Stopwatch.StartNew();
+            stopWatch.Start();
+            while (!ConnectionHelper.PingHost())
+            {
+                if (stopWatch.ElapsedMilliseconds >= 30000)
+                {
+                    stopWatch.Stop();
+                    //backendServerProcess.Kill();
+                    break;
+                }
+            }
 
-            //if (stopWatch.IsRunning)
-            //{
-            //    stopWatch.Stop();
-            //    DisplayRootViewFor<MainViewModel>();
-            //}
-            //else
-            //{
-            //    MessageBox.Show("Error! Unable to start the server", "Server Connection Error", MessageBoxButton.OK,MessageBoxImage.Error);
-            //    this.Application.Shutdown(1);
-            //}
+            if (stopWatch.IsRunning)
+            {
+                stopWatch.Stop();
+                DisplayRootViewFor<MainViewModel>();
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Error! Unable to start the server", "Server Connection Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                this.Application.Shutdown(1);
+            }
 
             DisplayRootViewFor<MainViewModel>();
         }
@@ -120,31 +100,9 @@ namespace FastPosFrontend
         {
             //EXCEPTION
 
-            //_backendServerProcess.Kill();
+            _backendServerProcess.Kill();
             base.OnExit(sender, e);
         }
 
-        private static void WriteProcessId(int id)
-        {
-            File.WriteAllText($"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}temp_pos.txt", $"{id}");
-        }
-
-        private static void ReadProcessId()
-        {
-            var stringId = File.ReadAllText($"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}temp_pos.txt");
-            ;
-            if (int.TryParse(stringId, out var id))
-            {
-                try
-                {
-                    var process = Process.GetProcessById(id);
-                    process.Kill();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-        }
     }
 }
